91bbe69d09010b413fa8b995c4bcce335923ff4a
shelly <141763804+ifoundcandy@users.noreply.github.com>
2024-10-10 16:59:32 -0300

    1

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,58 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'FNAF',
+    'Quais são os principais animatrônicos que aparecem no jogo original de Five Nights at Freddy's?',
+    'Freddy, Bonnie, Chica e Foxy'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'FNAF',
+    'Qual é a história por trás do restaurante Freddy Fazbeaz Pizza e como ela se conecta aos eventos dos jogos?',
+    'Freddy Fazbear Pizza é um restaurante familiar onde os animatrônicos se apresentam para entreter as crianças. A história é marcada por eventos trágicos, incluindo o desaparecimento de crianças, que estão relacionados a um serial killer, levando a uma série de acontecimentos sobrenaturais e assombrações.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'FNAF',
+    'Quais os nomes das seis crianças vítimas de William Afton?',
+    'Gabriel, Suzie, Jeremy, Fritz, Charlie e Cassidy'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'FNAF',
+    'Henry Emily construiu lefty com o intuito de:',
+    'Prender Puppet, que possuia sua filha, Charlie'
+
+criaCartao(
+    'Undertale',
+    'Quais as três rotas compostas no jogo?',
+    'Pacifista, Neutra e Genocida'
+)
+
+criaCartao(
+    'Undertale',
+    'Quais são as principais lições boas que a rota pacifista nos ensina?',
+    'Empatia, Compaixão, Amizade, Aceitação e Determinação'
+)
+
+criaCartao(
+    'Madoka Magica',
+    'Qual a garota mágica que tem magia temporal?',
+    'Homura Akemi'
+)
+
+criaCartao(
+    'Madoka Magica',
+    'Qual é o verdadeiro objetivo de Kyubey ao fazer contrato com garotas mágicas?',
+    'Seu objetivo ao fazer contratos com garotas mágicas é transformar suas esperanças em energia, que ele utiliza para combater a entropia no universo.'
+)
+
+criaCartao(
+    'Creative Squad',
+    'Qual foi o membro que conseguiu prender a entidade 1',
+    'Cadu'
+)
+
+criaCartao(
+    'Creative Squad',
+    'Quem eram os invasores na CS3?',
+    'Nait e Pedrux'
 )
